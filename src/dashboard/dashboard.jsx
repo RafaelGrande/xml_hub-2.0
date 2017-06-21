36bc0735e81a8ddbf8db8e213972a8cc77dcaa2f
Rafael Grande <rafaelgrande@outlook.com>
2017-06-21 19:16:29 -0300

    Criacao da tabela e botoes de navegacao

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -10,20 +10,35 @@ import DashboardList from './dashboardList'
 export default class Dashboard extends Component {
     constructor(props) {
         super(props)
-        this.state = {list: []}
+        this.state = {list: [], skip: 0}
+
+        this.handleNext = this.handleNext.bind(this)
 
         this.refresh()
     }
 
+    handleNext(soma) {
+        this.setState((prevState) => {
+            return {skip: prevState.skip + soma};
+        });
+        console.log(this.state.skip)
+        
+    }
+
     refresh(){
-        axios.get('http://db.jussilabs.com/carrefour/system.js/_eval?criteria={%22fn%22:%22getMetricsVeiculos(%272017-06-01%27,%272017-06-02%27,1)%22}')
+        console.log('refresh()')
+        console.log(this.state.skip)
+        axios.get('http://db.jussilabs.com/carrefour/system.js/_eval?criteria={%22fn%22:%22getMetricsVeiculos(%272017-06-01%27,%272017-06-02%27,'+this.state.skip+')%22}')
             .then(result => this.setState({...this.state, list: result.data.result._batch}))
     }
     render() {
         return (
             <div>
                 <h1>teste</h1>
-                <DashboardList list={this.state.list}/>
+                <DashboardList 
+                    list={this.state.list}
+                    handleNext={this.handleNext}
+                />
             </div>
         )  
     }
--- src/dashboard/dashboardList.jsx
+++ src/dashboard/dashboardList.jsx
@@ -8,24 +8,42 @@ export default props => {
         const list = props.list || []
         return list.map(metric => (
             <tr key={metric.clicks}>
-                <td>{metric.veiculo}</td>
+                <td className="mdl-data-table__cell--non-numeric">{metric.veiculo}</td>
                 <td>{metric.clicks}</td>
+                <td>{metric.conversao}</td>
+                <td>{metric.tx_conv}</td>
+                <td>{metric.ROI}</td>
+                <td>{metric.receita}</td>
+                <td>{metric.investimento}</td>
             </tr>
         ))
         
     }
 
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Nome</th>
-                    <th>Cliques</th>
-                </tr>
-            </thead>
-            <tbody>
-                {renderRows() }
-            </tbody>
-        </table>
+        <div>
+            <table className="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
+                <thead>
+                    <tr>
+                        <th className="mdl-data-table__cell--non-numeric mdl-data-table__header--sorted-ascending">Nome</th>
+                        <th>Cliques</th>
+                        <th>Converões</th>
+                        <th>Taxa de conversão</th>
+                        <th>ROI</th>
+                        <th>Receita</th>
+                        <th>investimento</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {renderRows() }
+                </tbody>
+            </table>
+            <button type="button" className="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab" onClick={() => props.handleNext(-10)}>
+                <i className="material-icons">keyboard_arrow_left</i>
+            </button>
+            <button type="button" className="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab" onClick={() => props.handleNext(10)}>
+                <i className="material-icons">keyboard_arrow_right</i>
+            </button>
+        </div>
     )
 }
